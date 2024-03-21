62091fbb72a3462d487ed119d93a503ee9bd6cd0
E-Bara <e-bara@outlook.com>
2024-03-21 15:26:23 +0100

    feat: Explicit Elastic index mapping

--- strategies/elastic_title_fuzziness_similarity_strategy.py
+++ strategies/elastic_title_fuzziness_similarity_strategy.py
@@ -8,6 +8,171 @@ ES_USER = "elastic"
 ES_INDEX = "test_elastic_fuzziness_similarity"
 ES_URL = "http://localhost:9200"
 
+ES_INDEX_MAPPING = {
+
+        "properties": {
+            "abstracts": {
+                "properties": {
+                    "language": {
+                        "type": "keyword",
+                    },
+                    "value": {
+                        "type": "text",
+                        "fields": {
+                            "keyword": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            }
+                        }
+                    }
+                }
+            },
+            "contributions": {
+                "properties": {
+                    "contributor": {
+                        "properties": {
+                            "name": {
+                                "type": "text",
+                                "fields": {
+                                    "keyword": {
+                                        "type": "keyword",
+                                        "ignore_above": 256
+                                    }
+                                }
+                            },
+                            "source": {
+                                "type": "keyword",
+                            },
+                            "source_identifier": {
+                                "type": "keyword",
+                            }
+                        }
+                    },
+                    "rank": {
+                        "type": "long"
+                    },
+                    "role": {
+                        "type": "keyword",
+                    }
+                }
+            },
+            "document_type": {
+                "properties": {
+                    "label": {
+                        "type": "keyword",
+                    },
+                    "uri": {
+                        "type": "keyword",
+                    }
+                }
+            },
+            "harvester": {
+                "type": "keyword",
+            },
+            "id": {
+                "type": "keyword",
+            },
+            "identifiers": {
+                "properties": {
+                    "type": {
+                        "type": "keyword",
+                    },
+                    "value": {
+                        "type": "keyword",
+                    }
+                }
+            },
+            "source_identifier": {
+                "type": "keyword",
+            },
+            "subjects": {
+                "properties": {
+                    "alt_labels": {
+                        "properties": {
+                            "language": {
+                                "type": "keyword",
+                            },
+                            "value": {
+                                "type": "text",
+                                "fields": {
+                                    "keyword": {
+                                        "type": "keyword",
+                                        "ignore_above": 256
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "pref_labels": {
+                        "properties": {
+                            "language": {
+                                "type": "text",
+                                "fields": {
+                                    "keyword": {
+                                        "type": "keyword",
+                                        "ignore_above": 256
+                                    }
+                                }
+                            },
+                            "value": {
+                                "type": "text",
+                                "fields": {
+                                    "keyword": {
+                                        "type": "keyword",
+                                        "ignore_above": 256
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "uri": {
+                        "type": "text",
+                        "fields": {
+                            "keyword": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            }
+                        }
+                    }
+                }
+            },
+            "subtitles": {
+                "properties": {
+                    "language": {
+                        "type": "keyword",
+                    },
+                    "value": {
+                        "type": "text",
+                        "fields": {
+                            "keyword": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            }
+                        }
+                    }
+                },
+
+            },
+            "titles": {
+                "properties": {
+                    "language": {
+                        "type": "keyword",
+                    },
+                    "value": {
+                        "type": "text",
+                        "fields": {
+                            "keyword": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+}
+
 
 class ElasticTitleFuzzinessSimilarityStrategy(SimilarityStrategy):
     def __init__(self):
@@ -17,7 +182,7 @@ class ElasticTitleFuzzinessSimilarityStrategy(SimilarityStrategy):
             verify_certs=True,
         )
         if not self.es.indices.exists(index=ES_INDEX):
-            self.es.indices.create(index=ES_INDEX)
+            self.es.indices.create(index=ES_INDEX, mappings=ES_INDEX_MAPPING)
 
     def load_reference(self, entity: Entity, reference: Reference):
         """
